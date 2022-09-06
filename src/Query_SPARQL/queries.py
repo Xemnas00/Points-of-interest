b2a2a6902d4c641007016af74e7187dc313d7e3c
Salvatore Leonardo <pizzutosalvatoreleonardo@gmail.com>
2022-09-07 00:39:36 +0200

    Added sparql queries

--- src/Query_SPARQL/queries.py
+++ src/Query_SPARQL/queries.py
@@ -1,24 +1,26 @@
 from os import environ
-from pathlib import Path
 import rdflib
-from rdflib import Graph, URIRef, Literal, Namespace, RDF
 
 base = environ["POI_LOCATION"]
 
 def queries():
-    query1()
-
-def query1():
     g = rdflib.Graph()
     g.bind("poi", "https://purl.archive.org/purl/net/poi_sardegna_sicilia/ontologia")
     g.bind("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
     g.bind("rdfs", "http://www.w3.org/2000/01/rdf-schema#")
-    g.bind("owl", "http://www.w3.org/2002/07/owl")
     g.parse(base + "/POI_RDF_TURTLE.ttl", format='turtle')
+    print("*" * 100)
+    query1(g)
+    print("*" * 100)
+    query2(g)
+    print("*" * 100)
+    query3(g)
+    print("*" * 100)
 
-    results = g.query("""
+#POIs ordered by price
+def query1(graph):
+    results = graph.query("""
         PREFIX poi: <https://purl.archive.org/purl/net/poi_sardegna_sicilia/ontologia>
-        PREFIX owl: <http://www.w3.org/2002/07/owl>
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     
@@ -27,10 +29,51 @@ def query1():
             ?punto rdf:type poi:Punto_di_interesse .
             ?punto rdfs:label ?denominazione .
             ?punto poi:prezzo ?prez .
-        }
+        } ORDER BY ?prez
     """)
 
-    print('Results!')
     for row in results:
         print(row)
 
+#POIs located in "Palermo"
+def query2(graph):
+    results = graph.query("""
+        PREFIX poi: <https://purl.archive.org/purl/net/poi_sardegna_sicilia/ontologia>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT ?punto
+        WHERE {
+            ?punto rdf:type poi:Punto_di_interesse ;
+                   rdfs:label ?denominazione ;
+                   poi:inIndirizzo ?indirizzo .
+            ?indirizzo poi:inComune ?comune .
+            ?comune rdfs:label ?nome_comune .
+            FILTER(?nome_comune="Palermo") .
+        }
+    """)
+
+    for row in results:
+        print(row)
+
+#Regions and their POIs' average price
+def query3(graph):
+    results = graph.query("""
+        PREFIX poi: <https://purl.archive.org/purl/net/poi_sardegna_sicilia/ontologia>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT ?regione (ROUND(AVG(?prez)*100)/100 AS ?average_price)
+        WHERE {
+            ?regione rdf:type poi:Regione ;
+                     poi:haComune ?comune .
+            ?comune poi:haIndirizzo ?indirizzo .
+            ?indirizzo poi:haPuntoDiInteresse ?poi .
+            ?poi poi:prezzo ?prez .
+        } 
+        GROUP BY ?regione
+        ORDER BY ?average_price
+    """)
+
+    for row in results:
+        print(row)
